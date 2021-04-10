fc84bd099336333c4380c08c596f1df69cb9bd32
Jegor Rysskin <jegorrysskin@gmail.com>
2021-04-10 17:11:05 +0200

    Added new 6800xt to amd-be

--- src/store/model/amd-be.ts
+++ src/store/model/amd-be.ts
@@ -83,6 +83,14 @@ export const AmdBe: Store = {
     {
       brand: 'amd',
       cartUrl:
+        'https://www.amd.com/en/direct-buy/5496921500/be?add-to-cart=true',
+      model: 'amd reference',
+      series: 'rx6800xt',
+      url: 'https://www.amd.com/en/direct-buy/5496921500/be',
+    },
+    {
+      brand: 'amd',
+      cartUrl:
         'https://www.amd.com/en/direct-buy/5458374200/be?add-to-cart=true',
       model: 'amd reference',
       series: 'rx6900xt',
