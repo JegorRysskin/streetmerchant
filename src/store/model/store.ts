9680dc037e6c781a3580a97b0039c420a5f4b401
Jegor Rysskin <jegorrysskin@gmail.com>
2021-04-24 23:49:43 +0200

    Merge branch 'main-j' into main

--- src/store/model/store.ts
+++ src/store/model/store.ts
@@ -92,6 +92,9 @@ export type Model =
   | 'eagle oc'
   | 'eagle'
   | 'ekwb'
+  | 'epic x'
+  | 'ex gamer'
+  | 'ex oc'
   | 'ex'
   | 'founders edition'
   | 'ftw3'
@@ -129,6 +132,7 @@ export type Model =
   | 'jetstream oc'
   | 'ko oc'
   | 'ko'
+  | 'master'
   | 'merc'
   | 'merc core'
   | 'merc ultra'
@@ -138,6 +142,7 @@ export type Model =
   | 'nitro+ oc se'
   | 'nitro oc se'
   | 'nitro oc'
+  | 'oc'
   | 'pegasus'
   | 'pegasus oc'
   | 'phantom'
@@ -180,6 +185,7 @@ export type Model =
   | 'ventus 3x'
   | 'vision oc'
   | 'vision'
+  | 'x trio'
   | 'xbox series s'
   | 'xbox series x'
   | 'xc black'
@@ -192,6 +198,7 @@ export type Model =
   | 'xlr8 gaming'
   | 'xlr8 revel'
   | 'xlr8 uprising'
+  | 'xtreme'
   | 'qick';
 
 export type Link = {
@@ -207,11 +214,18 @@ export type Link = {
   url: string;
 };
 
+export type CaptchaHandlerElements = {
+  image: string;
+  input: string;
+  submit: string;
+};
+
 export type LabelQuery = Element[] | Element | string[];
 
 export type Labels = {
   bannedSeller?: LabelQuery;
   captcha?: LabelQuery;
+  captchaHandler?: CaptchaHandlerElements;
   container?: string;
   inStock?: LabelQuery;
   outOfStock?: LabelQuery;
